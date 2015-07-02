d212871ade0ae498f5b8ae3a164ee74f5416127e
Tomasz Malisiewicz <tom@vision.ai>
2015-07-02 19:46:03 -0400

    Add a script to stop the deepdream server

---
+++ stop.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#
+# Start the docker container which will keep looking for images inside
+# the inputs/ directory and spew out results into outputs/
+
+docker stop deepdream-files deepdream-compute deepdream-json
+docker rm deepdream-files deepdream-compute deepdream-json
