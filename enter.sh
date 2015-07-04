111ba8a08f62ac6120a359062625b3782949f6c4
Tomasz Malisiewicz <tom@vision.ai>
2015-07-04 13:41:44 -0400

    Allow entering into containers when script is running and when it is not running

--- enter.sh
+++ enter.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 #A simple docker script to enter into the container with caffe and other shell scripts
-docker run -t -i --rm --name deepdream-enter --volumes-from=deepdream-compute visionai/clouddream /bin/bash
+
+if [ "`docker ps | grep deepdream-compute`" = "" ]; then
+    echo "Making volume"
+    docker run -t -i --rm --name deepdream-enter -v `pwd`/deepdream:/opt/deepdream visionai/clouddream /bin/bash
+else
+    echo "Steaming volume from deepdream-compute"
+    docker run -t -i --rm --name deepdream-enter --volumes-from=deepdream-compute visionai/clouddream /bin/bash
+fi
