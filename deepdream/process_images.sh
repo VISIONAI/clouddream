8eabbf1d5fb5a2210f086440746d69271a92a574
Tom Malisiewicz <tom@vision.ai>
2015-07-04 15:34:50 -0400

    Process_images.sh loop now calls process_images_once.sh

--- deepdream/process_images.sh
+++ deepdream/process_images.sh
@@ -3,28 +3,8 @@
 # We will be running this script inside the visionai/clouddream Docker image
 # Copyright vision.ai, 2015
 
-cd /opt/deepdream
-
 while [ true ];
 do
-    cd /opt/deepdream/inputs
-    find . -type f -not -path '*/\.*' -print0 | while read -d $'\0' f;
-    do
-	cd /opt/deepdream
-	if [ -e outputs/${f} ];
-	then
-	    echo "File ${f} already processed"
-	else
-	    echo "Deepdream" ${f}
-	    chmod gou+r inputs/${f}
-	    cp inputs/${f} input.jpg
-	    python deepdream.py
-	    ERROR_CODE=$?
-	    echo "Error Code is" ${ERROR_CODE}
-	    cp output.jpg outputs/${f}
-	    rm output.jpg
-	    echo "Just created" outputs/${f}
-	fi
-    done
+    ./process_images_once.sh
     sleep 1
 done
