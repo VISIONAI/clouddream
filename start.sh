99a29f346ec09fad71d6ee9bf774ec87d9bf6964
Tomasz Malisiewicz <tom@vision.ai>
2015-07-04 02:44:32 -0400

    Show how to change the output port to be something other than 80, useful when webserver is already running

--- start.sh
+++ start.sh
@@ -3,7 +3,8 @@
 # Start the docker container which will keep looking for images inside
 # the inputs/ directory and spew out results into outputs/
 
-docker run --name deepdream-files -v `pwd`/deepdream:/usr/share/nginx/html:ro -d -p 80:80 nginx
+PORT=80
+docker run --name deepdream-files -v `pwd`/deepdream:/usr/share/nginx/html:ro -d -p ${PORT}:80 nginx
 docker run --name deepdream-compute -v `pwd`/deepdream:/opt/deepdream -d visionai/clouddream /bin/bash -c "cd /opt/deepdream && ./process_images.sh 2>&1 > log.html"
 docker run --name deepdream-json --volumes-from deepdream-compute -d ubuntu:14.04 /bin/bash -c "cd /opt/deepdream && ./make_json.sh"
 
