18686e28ab1c902f0a9253c0a906d0cfc0f1a2cf
Tomasz Malisiewicz <tom@vision.ai>
2015-07-05 20:38:28 -0400

    Stop the deepdream-enter container

--- stop.sh
+++ stop.sh
@@ -3,5 +3,5 @@
 # Start the docker container which will keep looking for images inside
 # the inputs/ directory and spew out results into outputs/
 
-docker stop deepdream-files deepdream-compute deepdream-json
-docker rm deepdream-files deepdream-compute deepdream-json
+docker stop deepdream-files deepdream-compute deepdream-json deepdream-enter
+docker rm deepdream-files deepdream-compute deepdream-json deepdream-enter
