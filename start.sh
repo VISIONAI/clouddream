bbc24f893929b460014f8306ad30e55207e09af1
Tomasz Malisiewicz <tom@vision.ai>
2015-07-07 22:16:36 -0400

    Add start script which runs php instead of nginx, this is because the uploader is using php for incoming files

--- start.sh
+++ start.sh
@@ -3,8 +3,11 @@
 # Start the docker container which will keep looking for images inside
 # the inputs/ directory and spew out results into outputs/
 
+docker build -t deepdream-php - < ./uploaderDockerfile
+
 PORT=80
-docker run --name deepdream-files -v `pwd`/deepdream:/usr/share/nginx/html:ro -d -p ${PORT}:80 nginx
+docker run --name deepdream-files -v `pwd`/deepdream:/var/www/html/ -p ${PORT}:80  -d deepdream-php
+#docker run --name deepdream-files -v `pwd`/deepdream:/usr/share/nginx/html:ro -d -p ${PORT}:80 nginx
 docker run --name deepdream-compute -v `pwd`/deepdream:/opt/deepdream -d visionai/clouddream /bin/bash -c "cd /opt/deepdream && ./process_images.sh 2>&1 > log.html"
 docker run --name deepdream-json --volumes-from deepdream-compute -d ubuntu:14.04 /bin/bash -c "cd /opt/deepdream && ./make_json.sh"
 
