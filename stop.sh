efe526fccbaff0f807db5ebdaab3d12667f3a396
Jose Ochoa <jose-ochoa@mabuk.net>
2016-08-27 18:39:44 -0500

    Check if container is created/running before stopping/removing

--- stop.sh
+++ stop.sh
@@ -3,5 +3,19 @@
 # Start the docker container which will keep looking for images inside
 # the inputs/ directory and spew out results into outputs/
 
-docker stop deepdream-files deepdream-compute deepdream-json deepdream-enter
-docker rm deepdream-files deepdream-compute deepdream-json deepdream-enter
+CONTAINER=deepdream_enter
+STOP_CONTAINER=''
+REMOVE_CONTAINER=''
+TRUE='true'
+
+RUNNING=$(docker inspect --format="{{ .State.Running }}" $CONTAINER 2> /dev/null)
+
+if [ $? -eq 0 ]; then
+    REMOVE_CONTAINER=$CONTAINER
+    if [ "$RUNNING" = "$TRUE" ]; then
+        STOP_CONTAINER=$CONTAINER
+    fi
+fi
+
+docker stop deepdream-files deepdream-compute deepdream-json ${STOP_CONTAINER}
+docker rm deepdream-files deepdream-compute deepdream-json ${REMOVE_CONTAINER}
